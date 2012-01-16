6ab130abf3dfa99ada5fa189718d6dec50a4c34d
Alex <alex@recordsonribs.com>
2012-01-16 14:09:52 +0000

    Reformed this bit of license

--- wp-abstract.php
+++ wp-abstract.php
@@ -11,7 +11,6 @@ License: GPL2
 /*  
 Copyright 2012  Alex Andrews  (alex@recordsonribs.com)
 
-    it under the terms of the GNU General Public License, version 2, as 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as 
 published by the Free Software Foundation.
