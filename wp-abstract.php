486e64a82419256f1bfa9bb6fe13ce8f19a14dc9
conatus <alex@recordsonribs.com>
2012-05-24 18:57:56 +0100

    Have no idea what this variable was intended to do - remove!

--- wp-abstract.php
+++ wp-abstract.php
@@ -26,8 +26,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 class wp_abstract_post_type {
-	static $namespace = '';
-
 	public $name = '';
 	public $single = '';
 	public $plural = '';
