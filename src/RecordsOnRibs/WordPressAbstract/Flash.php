16add0295d201d48d3485262077636b38e0a9805
Alex <alex@recordsonribs.com>
2013-12-28 12:25:00 +0000

    Remove space before function call

--- src/RecordsOnRibs/WordPressAbstract/Flash.php
+++ src/RecordsOnRibs/WordPressAbstract/Flash.php
@@ -352,7 +352,7 @@ class Flash {
 		echo '<p>' . esc_html( $message ) . '</p>';
 
 		if ( $sticky ) {
-			echo '<p><a href="' . esc_url ( '?wpf_suppress_sticky=$id' ) . '">Don\'t show me this message again.</a></p>';
+			echo '<p><a href="' . esc_url( '?wpf_suppress_sticky=$id' ) . '">Don\'t show me this message again.</a></p>';
 		}
 
 		echo '</div>';
